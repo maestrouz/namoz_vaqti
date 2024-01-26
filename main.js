3c6bfe85e65416ab8beb75fd8cf56d70faf615da
maestrouz <tsrichpubgm@gmail.com>
2024-01-26 19:42:11 +0500

    up

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const box = document.querySelector(".box");
 const select = document.getElementById("select");
 let url = "https://islomapi.uz/api/present/day?region=Toshkent";
@@ -12,24 +11,53 @@ select.addEventListener("change", (e) => {
       const { tong_saharlik, quyosh, peshin, asr, shom_iftor, hufton } =
         data.times;
       box.innerHTML = `
+      <img class="line" src="./line.png" alt="line">
       <div class="time1">
       <div class="time_in"><h2>Tong: <h3>${tong_saharlik}</h3></h2></div>
     </div>
-    <div class="time2">
-      <h2>Quyosh: <h3>${quyosh}</h3> </h2>
+    <div class="time1">
+    <div class="time_in"><h2>Quyosh: </h2>
+    <h3>${quyosh}</h3></div>
     </div>
-    <div class="time3">
-      <h2>Peshin: <h3>${peshin}</h3> </h2>
+
+    <div class="time1">
+    <div class="time_in"><h2>Peshin: </h2> <h3>${peshin}</h3></div>
     </div>
-    <div class="time4">
-      <h2>Asr: <h3>${asr}</h3> </h2>
+
+    <div class="time1">
+    <div class="time_in"><h2>Asr:</h2> <h3>${asr}</h3></div>
     </div>
-    <div class="time5">
-      <h2>Shom: <h3>${shom_iftor}</h3> </h2>
+
+    <div class="time1">
+    <div class="time_in"><h2>Shom:</h2> <h3>${shom_iftor}</h3></div>
     </div>
-    <div class="time6">
-      <h2>Hufton: <h3>${hufton}</h3> </h2>
-    </div>`
+
+    <div class="time1">
+    <div class="time_in"><h2>Hufton: </h2> <h3>${hufton}</h3></div>
+    </div>`;
     });
 });
 
+
+  fetch('https://islomapi.uz/api/present/week')
+        .then(response => response.json())
+        .then(data => {
+            const namozVaqtlari = document.getElementById("namoz-vaqtlari");
+
+            data.forEach((item, index) => {
+                namozVaqtlari.innerHTML += `
+                    <div class="namoz-vaqtlari-item">
+                        <h3>${item.region}</h3>
+                        <p><strong>Sana:</strong> ${item.date}</p>
+                        <p><strong>Kun:</strong> ${item.weekday}</p>
+                        <p><strong>Tong Saharlik:</strong> ${item.times.tong_saharlik}</p>
+                        <p><strong>Quyosh:</strong> ${item.times.quyosh}</p>
+                        <p><strong>Peshin:</strong> ${item.times.peshin}</p>
+                        <p><strong>Asr:</strong> ${item.times.asr}</p>
+                        <p><strong>Shom Iftor:</strong> ${item.times.shom_iftor}</p>
+                        <p><strong>Hufton:</strong> ${item.times.hufton}</p>
+                    </div>
+                `;
+            });
+        })
+        .catch(error => console.error('Xatolik:', error));
